3e4179dcf6cb72ba3f02c79c20b4fa4cb5a1c1ad
Robert Fidler <robertf224@gmail.com>
2024-10-07 14:55:07 -0400

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,36 @@
-import puppeteer, { BrowserContext } from 'puppeteer';
-
+import puppeteer, { BrowserContext } from "puppeteer";
 import { ComputeModule } from "@palantir/compute-module";
 import { Type } from "@sinclair/typebox";
 
-const computeModule = new ComputeModule({
-  logger: console,
-  isAutoRegistered: false,
-  definitions: {
-    getWebpage: {
-        input: Type.Object({
-            url: Type.String({ format: "uri" }),
-        }),
-        output:  Type.String(),
-    }
-  },
-});
-
-(async () => {
+async function main(): Promise<void> {
     const browser = await puppeteer.launch();
 
-    computeModule
-        .register("getWebpage", async ({ url }) => {
-            let context: BrowserContext | undefined;
-            try {
-                context = await browser.createBrowserContext();
-                const page = await context.newPage();
-                await page.goto(url, { waitUntil: "networkidle2"});
-                return page.content();
-            } finally {
-                await context?.close();
-            }
-        })
-})()
+    const computeModule = new ComputeModule({
+        logger: console,
+        definitions: {
+          getWebpage: {
+              input: Type.Object({
+                  url: Type.String({ format: "uri" }),
+              }),
+              output:  Type.String(),
+          }
+        },
+      });
 
+    computeModule.register("getWebpage", async ({ url }) => {
+        let context: BrowserContext | undefined;
+        try {
+            context = await browser.createBrowserContext();
+            const page = await context.newPage();
+            await page.goto(url, { waitUntil: "networkidle2"});
+            return page.content();
+        } finally {
+            await context?.close();
+        }
+    });
+}
+
+main().catch(error => {
+    console.error(error);
+    process.exit(1);
+});
